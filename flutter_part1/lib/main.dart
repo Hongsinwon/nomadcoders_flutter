8ca59a9544a7af6310e77f9ba4a0527a4eb2f917
Hongsinwon <ghdtlsdnjs12@naver.com>
2023-02-24 16:49:38 +0900

    노마드코더 flutter Wallets APP UI

--- flutter_part1/lib/main.dart
+++ flutter_part1/lib/main.dart
@@ -1,24 +1,154 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_part1/widgets/Button.dart';
+import 'package:flutter_part1/widgets/currency_card.dart';
 
 void main() {
-  runApp(App());
+  runApp(const App());
 }
 
 class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: Scaffold(
-          appBar: AppBar(
-            elevation: 1,
-            centerTitle: true,
-            title: Text('Hello flutter'),
-          ),
-          body: Center(
-            child: Text('Hello world'),
-          ),
-        ),
+            backgroundColor: const Color(0xFF181818),
+            body: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: const [
+                            Text(
+                              'Hey, Selena',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 28,
+                                fontWeight: FontWeight.w800,
+                              ),
+                            ),
+                            Text(
+                              'Welcome back',
+                              style: TextStyle(
+                                // color: Colors.white.withOpacity(0.8),
+                                color: Color.fromRGBO(255, 255, 255, 0.5),
+                                fontSize: 18,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 70,
+                    ),
+                    Container(
+                      child: Text(
+                        'Total Balance',
+                        style: TextStyle(
+                          fontSize: 22,
+                          color: Colors.white.withOpacity(0.8),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    const Text(
+                      '\$5 194 482',
+                      style: TextStyle(
+                        fontSize: 45,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Button(
+                          text: 'Teansfer',
+                          bgColor: Color(0xFFF1B333),
+                          textcolor: Colors.black,
+                        ),
+                        Button(
+                          text: 'Request',
+                          bgColor: Color(0xff1f2123),
+                          textcolor: Colors.white,
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 100,
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          'Wallets',
+                          style: TextStyle(
+                            fontSize: 32,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Text(
+                          'View All',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.white.withOpacity(0.5),
+                          ),
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const CurrencyCard(
+                      name: 'Euro',
+                      code: 'EUR',
+                      amount: '6 428',
+                      icon: Icons.euro_rounded,
+                      isInverted: false,
+                    ),
+                    Transform.translate(
+                      offset: const Offset(0, -20),
+                      child: const CurrencyCard(
+                        name: 'Bitcoin',
+                        code: 'BTC',
+                        amount: '9 785',
+                        icon: Icons.currency_bitcoin,
+                        isInverted: true,
+                      ),
+                    ),
+                    Transform.translate(
+                      offset: const Offset(0, -40),
+                      child: const CurrencyCard(
+                        name: 'Dollar',
+                        code: 'USD',
+                        amount: '428',
+                        icon: Icons.attach_money_outlined,
+                        isInverted: false,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )),
       ),
     );
   }
